7a12053e023e618bb773cbf2be2dbcb80e5f0bde
Aleksandra Zobnina <aozobnina@vtb.ru>
2023-09-19 16:14:32 +0300

    init

---
+++ src/test/java/ru/neoflex/learning/creaditpipeline/deal/DealApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neoflex.learning.creaditpipeline.deal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
